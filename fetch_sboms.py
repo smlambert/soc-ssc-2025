78cce1710fe3cc65f75da0f155269e723f4fceda
Jana Hamdy <jabdelma@andrew.cmu.edu>
2025-07-22 20:30:37 +0300

    added parent project ids

--- fetch_sboms.py
+++ fetch_sboms.py
@@ -77,10 +77,18 @@ def fetch_sboms():
             path.write_text(sbom_resp.text)
 
             project_name = f"{PROJECT_ROOT} / {JAVA_VERSION} / {os_name} {arch} / jdk-{version}"
+            parent_uuids = {
+                "linux aarch64": "3680ceb0-702f-4ebd-811b-adece3f90a27", 
+                "linux x64": "6ca279d5-fb01-4957-9f85-de2ed07d3a69", 
+                "mac aarch64": "05904a4e-460d-4832-a4d5-4394cbec3c69", 
+                "mac x64": "b7752a97-4fdf-4c38-9557-89791eb11191", 
+                "windows x64": "14cf7d68-ca5c-4136-91bf-0a7d97ab3980",  
+            }
             metadata.append({
                 "path": str(path),
                 "projectName": project_name,
-                "projectVersion": version, 
+                "projectVersion": version,
+                "parentProject": parent_uuids.get(os_arch, "unknown"),
             })
             
     with open("metadata.json", "w") as f:
